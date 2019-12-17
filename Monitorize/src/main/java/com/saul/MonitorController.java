f0419f1114ce969d772890682fc791f4aa8a8d0e
sblanco5 <sblanco5@dxc.com>
2019-12-17 17:51:56 +0100

    commit

--- Monitorize/src/main/java/com/saul/MonitorController.java
+++ Monitorize/src/main/java/com/saul/MonitorController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,9 +33,17 @@ public class MonitorController {
 	    p.setDescripcion(desc);
 	    repoUsuarios.save(p);
 	  }
+	  
+	  @PostMapping("/createJsonUser")
+	  public User createJsonUser(@RequestBody User usr) {
+	   
+	    repoUsuarios.save(usr);
+	    
+	    return usr;
+	  }
 	 
-	  @RequestMapping("/listaUsers")
-	  @GetMapping
+
+	  @GetMapping("/listaUsers")
 	  public List<User> listaUsuarios() {
 	    
 	   return repoUsuarios.findAll();
